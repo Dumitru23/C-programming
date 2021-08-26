68f4a0671ae96c102f28dddb3447e792c63fe1cb
Mindrigan Dumitru <51724570+dumitru1111@users.noreply.github.com>
2021-08-26 18:42:37 -0400

    Add files via upload

---
+++ lab1/w1_home.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("*********************************\n");
+	printf("*** Welcome To C Programming ***\n");
+	printf("*********************************\n");
+	return 0;
+}
